1cb18b822ce74f2c7ec00b85ce6a09b2b973641d
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-09 12:45:08 +0530

    declaration of variables

---
+++ assignVariablesValues.groovy
@@ -0,0 +1,15 @@
+def age=20
+def name = 'gowtham'
+def pass = true
+
+
+// it will proint on the multiple lines
+println("age is :$age")
+println("name is :$name")
+println("pass is :$pass")
+
+
+//it will print on the single line only 
+print("age is :$age")
+print("name is :$name")
+print("pass is :$pass")
