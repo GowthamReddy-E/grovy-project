c412f92e7c126bf855893caf0ffab6336962df24
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-09 12:25:53 +0530

    numbers

---
+++ numbers.groovy
@@ -0,0 +1,13 @@
+
+int a=20
+b = 20
+def c = 20
+name = "gowtham"
+
+
+println(a.getClass().getName())
+println(b.getClass().getName())
+println(c.getClass().getName())
+
+println(name.getClass().getName())
+println(name.class) 
