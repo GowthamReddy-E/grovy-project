e6a38109650a52a2edb628b606bc3c8efa73abe8
gowthamReddy <gowthamreddy.6991@gmail.com>
2024-01-09 13:01:51 +0530

    data types

---
+++ dataTypes.groovy
@@ -0,0 +1,13 @@
+String name = 'gowtham'
+boolean itstrue = true
+
+int age = 26
+long phnu = 9381729345L
+double price = 20.4
+
+List array = [10,20,30,40]
+Map mapp = [name: 'gowtham',age: 26]
+Range range = 1..10
+
+
+
